b43675d8cb89c555e7f529b1ef7d595ecd7bf2e5
matthew winters <matthew winters>
2016-07-31 17:59:04 -0500

    no message

---
+++ PokemonTCGCollector/PokemonTCGCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokemonTCGCollector.Startup))]
+namespace PokemonTCGCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
